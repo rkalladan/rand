846d545bda3b0e913060e54c53175412dc7fe7d3
rahulkalladan <rahul.kalladan@timeinc.com>
2015-05-13 19:21:02 +0530

    Corrected many to many problems and included queries also....

--- src/main/java/com/rand/sprhib/annotation/service/EmployeeService.java
+++ src/main/java/com/rand/sprhib/annotation/service/EmployeeService.java
@@ -4,9 +4,15 @@ import java.util.List;
 
 import com.rand.sprhib.annotation.model.Empl;
 import com.rand.sprhib.annotation.model.Employee;
+import com.rand.sprhib.annotation.model.Role;
+import com.rand.sprhib.annotation.model.User;
 
 public interface EmployeeService {
 	public void insert(Employee employee);
 	public List<Employee> selectAll();
 	public void saveEmpl(Empl empl);
+	public void saveRole(Role role);
+	public void saveUser(User user);
+	public Role getRolename(long id);
+	public List<User> selectAllUsers();
 }
