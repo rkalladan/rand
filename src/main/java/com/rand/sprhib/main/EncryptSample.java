5684a316852062d77c8e339a3ee4246e2c3e3c41
rahulkalladan <rahul.kalladan@timeinc.com>
2015-05-22 20:11:32 +0530

    Created a class for Bcrypt encryption method...

---
+++ src/main/java/com/rand/sprhib/main/EncryptSample.java
@@ -0,0 +1,18 @@
+package com.rand.sprhib.main;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class EncryptSample {
+
+	public static void main(String[] args){
+		int i = 0;
+		while (i < 10) {
+			String password = "123456";
+			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+			String hashedPassword = passwordEncoder.encode(password);
+	 
+			System.out.println(hashedPassword);
+			i++;
+		}
+	}
+}
